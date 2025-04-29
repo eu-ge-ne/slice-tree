50c221689882f26d756e69bc311f107eaf2f1015
Eugene Pobochny <eu-ge-ne@users.noreply.github.com>
2025-04-29 18:58:53 -0400

    feat: add rb tree implementation (#9)
    * feat: add rb tree implementation
    
    * fix
    
    * fix

---
+++ insertion.ts
@@ -0,0 +1,89 @@
+import { bubble_metadata, NIL, Node, Tree } from "./node.ts";
+import { maximum, minimum } from "./querying.ts";
+import { left_rotate, right_rotate } from "./rotation.ts";
+
+export function insert_before(tree: Tree, parent: Node, child: Node): void {
+  if (parent.left === NIL) {
+    insert_left(tree, parent, child);
+  } else {
+    const x = maximum(parent.left);
+
+    insert_right(tree, x, child);
+  }
+}
+
+export function insert_after(tree: Tree, node: Node, child: Node): void {
+  if (node.right === NIL) {
+    insert_right(tree, node, child);
+  } else {
+    const x = minimum(node.right);
+
+    insert_left(tree, x, child);
+  }
+}
+
+export function insert_left(tree: Tree, parent: Node, child: Node): void {
+  if (parent === NIL) {
+    tree.root = child;
+    child.p = tree.root;
+  } else {
+    parent.left = child;
+    child.p = parent;
+  }
+
+  bubble_metadata(child);
+  insert_fixup(tree, child);
+}
+
+export function insert_right(tree: Tree, parent: Node, child: Node): void {
+  if (parent === NIL) {
+    tree.root = child;
+    child.p = tree.root;
+  } else {
+    parent.right = child;
+    child.p = parent;
+  }
+
+  bubble_metadata(child);
+  insert_fixup(tree, child);
+}
+
+function insert_fixup(tree: Tree, z: Node): void {
+  while (z.p.red) {
+    if (z.p === z.p.p.left) {
+      const y = z.p.p.right;
+      if (y.red) {
+        z.p.red = false;
+        y.red = false;
+        z.p.p.red = true;
+        z = z.p.p;
+      } else {
+        if (z === z.p.right) {
+          z = z.p;
+          left_rotate(tree, z);
+        }
+        z.p.red = false;
+        z.p.p.red = true;
+        right_rotate(tree, z.p.p);
+      }
+    } else {
+      const y = z.p.p.left;
+      if (y.red) {
+        z.p.red = false;
+        y.red = false;
+        z.p.p.red = true;
+        z = z.p.p;
+      } else {
+        if (z === z.p.left) {
+          z = z.p;
+          right_rotate(tree, z);
+        }
+        z.p.red = false;
+        z.p.p.red = true;
+        left_rotate(tree, z.p.p);
+      }
+    }
+  }
+
+  tree.root.red = false;
+}
---
+++ querying.ts
@@ -0,0 +1,96 @@
+import { NIL, Node } from "./node.ts";
+
+export function search(
+  x: Node,
+  i: number,
+): { node: Node; offset: number } | undefined {
+  while (x !== NIL) {
+    if (i < x.left_count) {
+      x = x.left;
+      continue;
+    } else {
+      i -= x.left_count;
+    }
+
+    if (i < x.count) {
+      return { node: x, offset: i };
+    } else {
+      i -= x.count;
+    }
+
+    x = x.right;
+  }
+
+  return;
+}
+
+export function search_line_position(x: Node, l: number): number | undefined {
+  let n = 0;
+
+  while (x !== NIL) {
+    if (l < x.left_line_count) {
+      x = x.left;
+      continue;
+    } else {
+      l -= x.left_line_count;
+      n += x.left_count;
+    }
+
+    if (l < x.lines.length) {
+      return n + x.lines[l]!;
+    } else {
+      l -= x.lines.length;
+      n += x.count;
+    }
+
+    x = x.right;
+  }
+
+  return;
+}
+
+export function minimum(x: Node): Node {
+  while (x.left !== NIL) {
+    x = x.left;
+  }
+
+  return x;
+}
+
+export function maximum(x: Node): Node {
+  while (x.right !== NIL) {
+    x = x.right;
+  }
+
+  return x;
+}
+
+export function predecessor(x: Node): Node {
+  if (x.left !== NIL) {
+    return maximum(x.left);
+  } else {
+    let y = x.p;
+
+    while (y !== NIL && x === y.left) {
+      x = y;
+      y = y.p;
+    }
+
+    return y;
+  }
+}
+
+export function successor(x: Node): Node {
+  if (x.right !== NIL) {
+    return minimum(x.right);
+  } else {
+    let y = x.p;
+
+    while (y !== NIL && x === y.right) {
+      x = y;
+      y = y.p;
+    }
+
+    return y;
+  }
+}
