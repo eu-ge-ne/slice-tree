43ca3b7cabe849a4ba5f3a8b6ddb287d591c1cc9
Eugene Pobochny <eu-ge-ne@users.noreply.github.com>
2025-05-13 13:43:27 -0400

    perf: buffer (#87)
    * perf: buffer
    
    * bump

--- src/buffer.ts
+++ src/buffer.ts
@@ -1,23 +1,15 @@
 import { create_eols, type EOL } from "./eol.ts";
 
-interface Pool {
-  readonly buffers: Buffer[];
-}
-
 export interface Buffer {
   text: string;
   readonly eols: EOL[];
 }
 
-export function create_buffer(pool: Pool, text: string): Buffer {
-  const buffer = {
+export function create_buffer(text: string): Buffer {
+  return {
     text,
     eols: create_eols(text).toArray(),
   };
-
-  pool.buffers.push(buffer);
-
-  return buffer;
 }
 
 export function grow_buffer(buffer: Buffer, text: string): void {
