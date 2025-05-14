cd309b0b55e1a352c30b8eabbaa7ad5f19cd3bf2
Eugene Pobochny <eugene.pobochny@gmail.com>
2025-05-14 00:49:10 -0400

    fix

---
+++ bench/insertion-seq.bench.ts
@@ -0,0 +1,49 @@
+import { SliceTree } from "../src/tree.ts";
+
+for (let power = 2; power < 5; power += 1) {
+  const n = 10 ** power;
+
+  Deno.bench(
+    `Inserting ${n} chars sequentially into a SliceTree`,
+    {
+      group: `Insertion sequential x${n}`,
+      baseline: true,
+    },
+    (b) => {
+      const text = new SliceTree(" ");
+      text.write(text.count, " ");
+
+      let pos = 1;
+
+      b.start();
+
+      for (let i = 0; i < n; i += 1) {
+        text.write(pos, crypto.randomUUID());
+        pos += 36;
+      }
+
+      b.end();
+    },
+  );
+
+  Deno.bench(
+    `Inserting ${n} chars sequentially into a string`,
+    {
+      group: `Insertion sequential x${n}`,
+    },
+    (b) => {
+      let text = " " + " ";
+
+      let pos = 1;
+
+      b.start();
+
+      for (let i = 0; i < n; i += 1) {
+        text = text.slice(0, pos) + crypto.randomUUID() + text.slice(pos);
+        pos += 36;
+      }
+
+      b.end();
+    },
+  );
+}
