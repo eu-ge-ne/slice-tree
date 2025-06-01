1a78eba412eb401f4ad0881bf96278ea2702e698
Eugene Pobochny <eugene.pobochny@gmail.com>
2025-06-01 00:34:58 -0400

    fix

--- test/create.test.ts
+++ test/create.test.ts
@@ -14,16 +14,11 @@ Deno.test("Create empty", () => {
 });
 
 Deno.test("Create with content", () => {
-  const text = new SliceTree(
-    "Lorem\nipsum\ndolor\nsit\namet,\nconsectetur\nadipiscing\nelit,\nsed\ndo\neiusmod\ntempor\nincididunt\nut\nlabore\net\ndolore\nmagna\naliqua.",
-  );
+  const text = new SliceTree("Lorem\nipsum\ndolor\nsit\namet");
 
-  assert_iterator(
-    text.read(0),
-    "Lorem\nipsum\ndolor\nsit\namet,\nconsectetur\nadipiscing\nelit,\nsed\ndo\neiusmod\ntempor\nincididunt\nut\nlabore\net\ndolore\nmagna\naliqua.",
-  );
-  assertEquals(text.count, 123);
-  assertEquals(text.line_count, 19);
+  assert_iterator(text.read(0), "Lorem\nipsum\ndolor\nsit\namet");
+  assertEquals(text.count, 26);
+  assertEquals(text.line_count, 5);
 
   assert_tree(text);
 });
