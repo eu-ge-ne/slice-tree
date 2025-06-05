b5b8d38a186b29b12718ffabc4d6c49b044edad7
Eugene Pobochny <eugene.pobochny@gmail.com>
2025-06-05 15:16:04 -0400

    fix

--- test/erase-from-line.test.ts
+++ test/erase-from-line.test.ts
@@ -3,9 +3,29 @@ import { assertEquals } from "@std/assert";
 import { SliceTree } from "../src/tree.ts";
 import { assert_iterator, assert_tree } from "./assert.ts";
 
-Deno.test("Erase from line", () => {
-  const text = SliceTree.of_code_units(
-    "Lorem😄\nipsum😄\ndolor😄\nsit😄\namet😄",
+Deno.test("Erase from line of code_units", () => {
+  const text = SliceTree.of_code_units("Lorem \nipsum \ndolor \nsit \namet");
+
+  assertEquals(text.line_count, 5);
+
+  text.erase_from_line(3, 0);
+
+  assert_iterator(text.read(0), "Lorem \nipsum \ndolor \n");
+  assertEquals(text.count, 21);
+  assertEquals(text.line_count, 4);
+
+  text.erase_from_line(1, 0);
+
+  assert_iterator(text.read(0), "Lorem \n");
+  assertEquals(text.count, 7);
+  assertEquals(text.line_count, 2);
+
+  assert_tree(text);
+});
+
+Deno.test("Erase from line of code_points", () => {
+  const text = SliceTree.of_code_points(
+    "Lorem😄\nipsum😄\ndolor😄\nsit😄\namet",
   );
 
   assertEquals(text.line_count, 5);
