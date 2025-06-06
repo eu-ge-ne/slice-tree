b33647e1b39efdd14c3fea60bbed68add3aae368
Eugene Pobochny <eugene.pobochny@gmail.com>
2025-06-06 00:44:00 -0400

    fix

--- test/create.test.ts
+++ test/create.test.ts
@@ -13,22 +13,56 @@ Deno.test("Create empty", () => {
   assert_tree(text);
 });
 
-Deno.test("Create with units", () => {
-  const text = SliceTree.units("Lorem \nipsum \ndolor \nsit \namet");
+Deno.test("Create as units", () => {
+  const text1 = SliceTree.units("A");
+  const text2 = SliceTree.units("😄");
+  const text3 = SliceTree.units("🤦🏼‍♂️");
 
-  assert_iterator(text.read(0), "Lorem \nipsum \ndolor \nsit \namet");
-  assertEquals(text.count, 30);
-  assertEquals(text.line_count, 5);
+  assert_iterator(text1.read(0), "A");
+  assert_iterator(text2.read(0), "😄");
+  assert_iterator(text3.read(0), "🤦🏼‍♂️");
 
-  assert_tree(text);
+  assertEquals(text1.count, 1);
+  assertEquals(text2.count, 2);
+  assertEquals(text3.count, 7);
+
+  assert_tree(text1);
+  assert_tree(text2);
+  assert_tree(text3);
 });
 
-Deno.test("Create with points", () => {
-  const text = SliceTree.points("Lorem😄\nipsum😄\ndolor😄\nsit😄\namet");
+Deno.test("Create as points", () => {
+  const text1 = SliceTree.points("A");
+  const text2 = SliceTree.points("😄");
+  const text3 = SliceTree.points("🤦🏼‍♂️");
 
-  assert_iterator(text.read(0), "Lorem😄\nipsum😄\ndolor😄\nsit😄\namet");
-  assertEquals(text.count, 30);
-  assertEquals(text.line_count, 5);
+  assert_iterator(text1.read(0), "A");
+  assert_iterator(text2.read(0), "😄");
+  assert_iterator(text3.read(0), "🤦🏼‍♂️");
 
-  assert_tree(text);
+  assertEquals(text1.count, 1);
+  assertEquals(text2.count, 1);
+  assertEquals(text3.count, 5);
+
+  assert_tree(text1);
+  assert_tree(text2);
+  assert_tree(text3);
+});
+
+Deno.test("Create as graphemes", () => {
+  const text1 = SliceTree.graphemes("A");
+  const text2 = SliceTree.graphemes("😄");
+  const text3 = SliceTree.graphemes("🤦🏼‍♂️");
+
+  assert_iterator(text1.read(0), "A");
+  assert_iterator(text2.read(0), "😄");
+  assert_iterator(text3.read(0), "🤦🏼‍♂️");
+
+  assertEquals(text1.count, 1);
+  assertEquals(text2.count, 1);
+  assertEquals(text3.count, 1);
+
+  assert_tree(text1);
+  assert_tree(text2);
+  assert_tree(text3);
 });
