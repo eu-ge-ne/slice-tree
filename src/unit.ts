39dfba64b59249e69dc2444c4234bc54ed7903b9
Eugene Pobochny <eugene.pobochny@gmail.com>
2025-06-08 15:20:43 -0400

    fix

--- src/unit.ts
+++ src/unit.ts
@@ -19,24 +19,16 @@ class UnitBuffer extends Buffer {
   }
 
   append(text: string): void {
-    this.#append_eols(text, this.len, this.eol_starts, this.eol_ends);
+    for (const x of text.matchAll(this.#regexp)) {
+      this.eol_starts.push(this.len + x.index);
+      this.eol_ends.push(this.len + x.index + x[0].length);
+    }
+
     this.len += text.length;
     this.#text += text;
   }
 
-  read(index: number, n: number): IteratorObject<string> {
-    return this.#text.slice(index, index + n)[Symbol.iterator]();
-  }
-
-  #append_eols(
-    text: string,
-    index: number,
-    starts: number[],
-    ends: number[],
-  ): void {
-    for (const x of text.matchAll(this.#regexp)) {
-      starts.push(index + x.index);
-      ends.push(index + x.index + x[0].length);
-    }
+  read(i: number, n: number): IteratorObject<string> {
+    return this.#text.slice(i, i + n)[Symbol.iterator]();
   }
 }
